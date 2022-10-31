4eb7cbbf26dccdc0b2df58b8a4601a97b24435de
Behenck <denilsontrespa10@gmail.com>
2022-10-31 12:03:49 -0300

    add to storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,30 @@
-import { colors } from '@ignite-ui/tokens'
+import { styled } from './styles'
+import { ComponentProps } from 'react'
 
-export function App() {
-  return <h1>Hello World!</h1>
-}
+export const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite300',
+  borderRadius: '$md',
+  border: 0,
+  fontWeight: 'bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
+})
+
+export type ButtonProps = ComponentProps<typeof Button>
